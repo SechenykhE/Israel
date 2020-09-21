036e4cb92433333a9e487a024d6c7eb56caf94f1
Elena Sechenykh <sechenyh@inbox.ru>
2020-09-21 18:34:51 +0300

    Добавляет шапку и главный экран, а такжечастично модальное окно "Заказ звонка"

---
+++ build/js/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+(function () {
+  /* var headerMain = document.querySelector('.header-nav');
+  var button = document.querySelector('.header-nav button');
+  var body = document.querySelector('body');
+
+  headerMain.classList.remove('header-nav--nojs');
+
+  button.addEventListener('click', function () {
+    if (headerMain.classList.contains('header-nav--closed')) {
+      headerMain.classList.remove('header-nav--closed');
+      headerMain.classList.add('header-nav--opened');
+      body.classList.add('hidden');
+    } else {
+      headerMain.classList.add('header-nav--closed');
+      headerMain.classList.remove('header-nav--opened');
+      body.classList.remove('hidden');
+    }
+  });*/
+
+  window.vendor.svg4everybody();
+})();
